d86d7c4ed2657f6dab6f38bd181c8ae61b0ad7bf
Gray Marchese <marchesegray@gmail.com>
2018-04-25 15:40:41 -0600

    Implemented KL divergence with loggamma function. Working.

--- test.py
+++ test.py
@@ -3,16 +3,24 @@ from vb import *
 
 ts = generate_ar(300,[.2,0,-.4,-.2],1,500)
 W, s = least_squares(ts,4)
-print('Maximum Liklihood Estimates: \n', W, '\n', s)
+#print('Maximum Liklihood Estimates: \n', W, '\n', s)
 
 
-noise_b = 1000
-noise_c = 0.001
-weight_b = 1000
-weight_c = 0.001
+noise_b = 10
+noise_c = 0.01
+weight_b = 10
+weight_c = 0.1
 prec = s*np.identity(4)
 X, Y = get_data(ts,4)
-for iteration in range(4000):
-    W, prec, weight_b, weight_c, noise_b, noise_c = post_params(X,Y,W,prec,weight_b,weight_c,noise_b,noise_c)
 
-print('Variational Bayesian Estimates: \n', W, '\n', 1/(noise_b*noise_c))
+print('N = '+ str(X.shape[0]))
+F_prev = 0.001
+for iteration in range(4):
+    updated = vb_ar_update(X,Y,W,prec,weight_b,weight_c,noise_b,noise_c)
+    W, prec, weight_b, weight_c, noise_b, noise_c, F = updated
+    if abs(F - F_prev)/abs(F_prev) - 1 < 0.01 :
+        print('Broke early at iteration'+str(iteration))
+        break
+    F_prev = F
+
+#print('Variational Bayesian Estimates: \n', W, '\n', 1/(noise_b*noise_c))
--- vb.py
+++ vb.py
@@ -1,23 +1,58 @@
 import numpy as np
 from numpy import linalg, trace as tr
+from math import log, pi
+from scipy.special import gamma, digamma
 
-def noise_prec_params(n, w, X, prec, noise_b, noise_c):
-    post_noise_b = 1/(0.5*linalg.norm(w)**2 + 0.5*tr(prec*X.transpose()*X)+ 1/noise_b)
-    post_noise_c = (n/2 + noise_c)
-    return post_noise_b, post_noise_c
+def vb_ar_update(X, Y, w, prec, weight_b, weight_c, noise_b, noise_c):
 
-def weight_prec_params(p, w, prec, weight_b, weight_c):
-    post_weight_b = 1/(0.5*linalg.norm(w)**2 + 0.5*tr(prec)+ 1/weight_b)
-    post_weight_c = (p/2 + weight_c)
-    return post_weight_b, post_weight_c
+    #Upper case is used for the posterior values and data.
+    #Lower case is used for the priors and constants.
+    n = X.shape[0]
+    p = X.shape[1]
 
-def post_params(X, Y, w, prec, weight_b, weight_c, noise_b, noise_c):
-    post_noise_b, post_noise_c = noise_prec_params(X.shape[0],w,X,prec,noise_b,noise_c)
-    post_noise_prec = post_noise_b*post_noise_c
-    post_weight_b, post_weight_c = weight_prec_params(X.shape[1],w,prec,weight_b,weight_c)
-    post_weight_prec = post_weight_b*post_weight_c
-    post_prec = linalg.inv(post_noise_prec*X.transpose()*X + post_weight_prec*np.identity(X.shape[1]))
-    post_w = post_prec*X.transpose()*post_noise_prec*Y
-    return post_w, post_prec, post_weight_b, post_weight_c, post_noise_b, post_noise_c
+    #A value used in the noise posterior and convergence criteria
+    E_D = 0.5*linalg.norm(w)**2 + 0.5*tr(prec*X.transpose()*X)
 
-def variational_bayes()
+    #Update noise precision gamma posterior
+    NOISE_B = 1/(E_D+1/noise_b)
+    NOISE_C = (n/2 + noise_c)
+    NOISE_PREC = NOISE_B*NOISE_C
+
+    #Update weight precision gamma posterior
+    WEIGHT_B = 1/(0.5*linalg.norm(w)**2 + 0.5*tr(prec)+ 1/weight_b)
+    WEIGHT_C = (p/2 + weight_c)
+    WEIGHT_PREC = WEIGHT_B*WEIGHT_C
+
+    #Update wieght and covariance matrix posterior means.
+    PREC = linalg.inv(NOISE_PREC*X.transpose()*X + WEIGHT_PREC*np.identity(p))
+    W = PREC*X.transpose()*NOISE_PREC*Y
+
+    #VB Lower Bound
+    beta = digamma(NOISE_C) + log(NOISE_B)
+    lwr_bnd = n*beta/2 - NOISE_PREC*E_D  - n*log(2*pi)/2
+
+    #POSTERIOR/Prior KL divergences
+    kl_w = kl_gaussian(W, PREC, w, prec)
+    kl_weight = kl_gamma(WEIGHT_B, WEIGHT_C, weight_b, weight_c)
+    kl_noise = kl_gamma(NOISE_B, NOISE_C, noise_b, noise_c)
+
+    #Negative Free-Energy
+    F = lwr_bnd - kl_w - kl_weight - kl_noise
+
+    return (W, PREC, WEIGHT_B, WEIGHT_C, NOISE_B, NOISE_C, F)
+
+def kl_gaussian( MU, COV, mu, cov):
+    d = mu.shape[0]
+    cov_inv = linalg.inv(cov)
+    kl_div = 0.5*log(linalg.det(cov)/linalg.det(COV))
+    kl_div = kl_div + 0.5*tr(cov_inv*COV)
+    kl_div = kl_div + 0.5*(mu - MU).transpose()*cov_inv*(mu - MU)
+    kl_div = kl_div - 0.5*d
+    return kl_div
+
+def kl_gamma(B,C,b,c):
+    kl_div = (C-1)*digamma(C) - log(B) - C - log(gamma(C)) 
+    kl_div = kl_div + log(gamma(c)) + c*log(b)
+    kl_div = kl_div - (c -1)*(digamma(C) + log(B)) + (B*C)/b
+    return kl_div
+
